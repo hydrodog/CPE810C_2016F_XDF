df7f6cc7b0fbbf3433f2f053eb9e50dbd8cf53b7
JayZejianZhou <zzhou12@stevens.edu>
2016-11-13 23:27:38 -0500

    try to undate PDF checkbox

--- Print/pprint.cpp
+++ Print/pprint.cpp
@@ -8,41 +8,7 @@ PPrint::PPrint() {
 }
 
 void PPrint::PProcess(){
-    QPrinter printer(QPrinter::HighResolution);
-    QPrintDialog dialog(&printer);
-    dialog.setWindowTitle("Print Document");
-#if 0
-    if(isReverse){//if you wanna print from end to begin
-        if(isPDF){}//TODO: is pdf? I need to deal with paint class now @Zejian
-
-        if(dialog.exec()==QDialog::Accepted){
-            printer.setOutputFormat(QPrinter::NativeFormat);
-           // printer.setOutputFileName("print.pdf");
-            QPainter painter;
-            painter.begin(&printer);
-            for(int PageNum=endPageNum;PageNum>=startPageNum;PageNum--){
-                painter.drawText(1000, 1000, "LET's FUJI CO.LTD 6666 哈哈哈哈");//Just test code in case other team fuck up
-                if(PageNum!=startPageNum)
-                    printer.newPage();
-            }
-            painter.end();
-        }
-    else{
-#endif
-          if(dialog.exec()==QDialog::Accepted){              
-              }
-          printer.setOutputFormat(QPrinter::NativeFormat);
-         // printer.setOutputFileName("print.pdf");
-          QPainter painter;
-          painter.begin(&printer);
-          for(int PageNum=0;PageNum<=5;PageNum++){
-              painter.drawText(1000, 1000, "LET's FUJI CO.LTD 6666 55哈哈哈");//Just test code in case other team fuck up
-              if(PageNum!=endPageNum)
-                  printer.newPage();
-              painter.end();
-            }
-//        }
- //  }
+    print();
 }
 
 void PPrint::setEndPageNum(int num){
@@ -71,5 +37,70 @@ void PPrint::setIsPDF(bool is)
 }
 
 void PPrint::print(){
+    QPrinter printer(QPrinter::HighResolution);
+    QPrintDialog dialog(&printer);
+    dialog.setWindowTitle("Print Document");
+    if (isPDF){
+        printer.setOutputFormat(QPrinter::PdfFormat);
+        printer.setOutputFileName("printXDF.pdf");
+    }
+    else{
+        printer.setOutputFormat(QPrinter::NativeFormat);
+        if(dialog.exec()==QDialog::Accepted){ //what printer do you wanna use
+            }
+    }
+    if(isReverse){
+        QPainter painter;
+        painter.begin(&printer);
+        for(int PageNum=endPageNum;PageNum>=startPageNum;PageNum--){
+            painter.drawText(1000, 1000, "LET's FUJI CO.LTD 6666 哈哈哈哈");//Just test code in case other team fuck up
+            if(PageNum!=startPageNum)
+                printer.newPage();
+        }
+        painter.end();
+    }
+    else {
+        QPainter painter;
+        painter.begin(&printer);
+        for(int PageNum=startPageNum;PageNum<=endPageNum;PageNum++){
+            painter.drawText(1000, 1000, "LET's FUJI CO.LTD 6666 55哈哈哈");//Just test code in case other teams fuck up
+            if(PageNum!=endPageNum)
+                printer.newPage();
+            painter.end();
+          }
+    }
 
+#if 0
+    if(isReverse){//if you wanna print from end to begin
+        if(isPDF){}//TODO: is pdf? I need to deal with paint class now @Zejian
+
+        if(dialog.exec()==QDialog::Accepted){
+            printer.setOutputFormat(QPrinter::NativeFormat);
+           // printer.setOutputFileName("print.pdf");
+            QPainter painter;
+            painter.begin(&printer);
+            for(int PageNum=endPageNum;PageNum>=startPageNum;PageNum--){
+                painter.drawText(1000, 1000, "LET's FUJI CO.LTD 6666 哈哈哈哈");//Just test code in case other team fuck up
+                if(PageNum!=startPageNum)
+                    printer.newPage();
+            }
+            painter.end();
+        }
+    else{
+
+          if(dialog.exec()==QDialog::Accepted){
+              }
+          printer.setOutputFormat(QPrinter::NativeFormat);
+         // printer.setOutputFileName("print.pdf");
+          QPainter painter;
+          painter.begin(&printer);
+          for(int PageNum=0;PageNum<=5;PageNum++){
+              painter.drawText(1000, 1000, "LET's FUJI CO.LTD 6666 55哈哈哈");//Just test code in case other team fuck up
+              if(PageNum!=endPageNum)
+                  printer.newPage();
+              painter.end();
+            }
+//        }
+ //  }
+#endif
 }
