bc6350e3ecd795badd4cb285d70ac5e67d6111dc
Seito Ryu <sryu@stevens.edu>
2016-11-22 01:01:40 -0500

    fix bugs

--- 2dgraphics/source/ExceptionTwoDG.cc
+++ 2dgraphics/source/ExceptionTwoDG.cc
@@ -1,12 +1,9 @@
 #include "ExceptionTwoDG.hh"
 
-ExceptionTwoDG::ExceptionTwoDG(std::string val): errCnt(val) {}
-friend std::ostream& ExceptionTwoDG::operator << (std::ostream& s, const ExceptionTwoDG& e){
+std::ostream& ExceptionTwoDG::operator << (std::ostream& s, const ExceptionTwoDG& e){
   return s << "Error: " << e.errCnt << ". When processing 2dgraphics!" << "\n";
 }
 
 std::string ExceptionTwoDG::getCng() const{
   return errCnt;
 }
-
-#endif
