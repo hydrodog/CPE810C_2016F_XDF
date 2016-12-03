d34fe4456fc25ac33e0aaf25885277e25589eb12
zchenpds <zchenpds@gmail.com>
2016-12-03 13:10:24 -0500

    Minor change

---
+++ pdf/podofo/tools/xdfimport/pdfgraphics.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: Zhuo Chen, Yingting Huang
+ */
+
+#include "pdfgraphics.h"
+
+PdfGState::PdfGState()//: m_CTM()
+{
+
+}
---
+++ pdf/podofo/tools/xdfimport/pdfgraphics.h
@@ -0,0 +1,29 @@
+/*
+ * Author: Zhuo Chen, Yingting Huang
+ */
+#ifndef PDFGRAPHICS_H__
+#define PDFGRAPHICS_H__
+
+#include <stack>
+#include <vector>
+#include <QPen>
+
+
+class PdfGState{
+    PdfGState();
+
+
+private:
+    // The current transformation matrix. It maps positions from user coordinates
+    // to device coordinates.
+    std::vector<double> m_CTM;
+
+
+    QPen m_qpen;
+};
+
+typedef std::stack<PdfGState> stackPdfGState_t;
+
+
+
+#endif
