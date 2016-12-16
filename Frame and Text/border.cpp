3735079c50e56f5af798209d631e06003850c965
Zayn <zchen35@stevens.edu>
2016-12-16 16:16:12 -0500

    Delete useless

--- Frame and Text/border.cpp
+++
@@ -1,8 +0,0 @@
-#include "border.h"
-#include<iostream>
-using namespace std;
-Border::Border(int height, int width):m_leftEdge(width/10),m_rightEdge(width/10),m_topEdge(height/10),m_downEdge(height/10)
-{}
-
-void Border::clear(){
-}
